ff822fc241e76cc9e487f3e3addae9785b6e2b94
Stephan Classen <st.classen@gmx.ch>
2020-06-16 11:02:03 +0200

    add examples for triggers

--- graylog-sample/server-spring-logback/src/main/java/module-info.java
+++ graylog-sample/server-spring-logback/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module dev.rico.samples.http.server.spring {
     requires dev.rico.logback.appender;
     requires spring.boot;
     requires spring.web;
+    requires spring.context;
     requires spring.beans;
     requires org.slf4j;
 }
